57e1a6e5d6bec250982475cf8de099fe1e1b92f3
AlexMochalin <alemochalin@yandex.ru>
2018-06-21 23:50:58 +0300

    Изменено название массива

--- chessboard/code3.js
+++ chessboard/code3.js
@@ -59,19 +59,19 @@ function chessBoard() {
 					td.innerText = latters[tdCounter];
 				};
 				if (trCounter == 1 && tdCounter !=0 && tdCounter !=9){
-					td.innerText = chessPawnWords[tdCounter];//Указание букв белых фигур
+					td.innerText = chessWords[tdCounter];//Указание букв белых фигур
 					td.style.color = 'white'; //Цвет белых букв
 				};
 				if (trCounter == 2 && tdCounter !=0 && tdCounter !=9){
-					td.innerText = chessPawnWords[0];//Указание пешек белых фигур
+					td.innerText = chessWords[0];//Указание пешек белых фигур
 					td.style.color = 'white'; //Цвет белых букв
 				};
 				if (trCounter == 8 && tdCounter !=0 && tdCounter !=9){
-					td.innerText = chessPawnWords[tdCounter];//Указание букв черных фигур
+					td.innerText = chessWords[tdCounter];//Указание букв черных фигур
 					td.style.color = 'black'; //Цвет белых букв
 				};
 				if (trCounter == 7 && tdCounter !=0 && tdCounter !=9){
-					td.innerText = chessPawnWords[0];//Указание пешек черных фигур
+					td.innerText = chessWords[0];//Указание пешек черных фигур
 					td.style.color = 'black'; //Цвет черных букв
 				};
 			}
